612a29123247c42595a2955365a80d46c5415357
vatssahil01 <113243664+vatssahil01@users.noreply.github.com>
2026-01-19 18:15:22 +0530

    Setup and make schema for auth

---
+++ Backend/src/app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./config/db.js"
+
+dotenv.config();
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+const PORT = process.env.PORT
+
+app.get("/", (req,res)=>{
+    res.send("Project Management System Api running")
+})
+
+app.listen(PORT, (req,res)=>{
+    console.log(`Server is working at PORT https://localhost:${PORT}`)
+})
+
+connectDB()
---
+++ Backend/src/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+
+        role: {
+            type: String,
+            enum: ["student", "teacher", "admin"],
+            default: "student"
+        },
+
+        isActive: {
+            type: Boolean,
+            default: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
