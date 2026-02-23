0e34aac0972b10148b6a6245bfbcbbde17bf8935
vatssahil01 <vatssahil003@gmail.com>
2026-02-23 17:14:02 +0530

    models fileSchema

--- Backend/src/models/File.js
+++ Backend/src/models/File.js
@@ -1,27 +1,27 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const fileSchema = new mongoose.Schema(
-//     {
-//         filename: String,
-//         originalname: String,
-//         mimetype: String,
-//         size: Number,
+const fileSchema = new mongoose.Schema(
+    {
+        filename: String,
+        originalname: String,
+        mimetype: String,
+        size: Number,
 
-//         project: {
-//             type: mongoose.Schema.Types.ObjectId,
-//             ref: "Project",
-//             required: true
-//         },
+        project: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Project",
+            required: true
+        },
 
-//         uplodedBy: {
-//             type: mongoose.Schema.Types.ObjectId,
-//             ref: "User",
-//             required: true
-//         }
-//     },
-//     { timestamps: true}
-// );
+        uplodedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    },
+    { timestamps: true}
+);
 
-// export default mongoose.model("File", fileSchema);
+export default mongoose.model("File", fileSchema);
 
 // After this step 4
