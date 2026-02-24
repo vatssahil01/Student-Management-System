c3d420bbd1c02f05efd5a0fbbade8a2d6fc42678
vatssahil01 <vatssahil003@gmail.com>
2026-02-24 16:26:17 +0530

    update file.js & upload.middleware.js

---
+++ Backend/src/middleware/upload.middleware.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "src/uploads/projects");
+    },
+    filename: function (req, file, cb) {
+        const uniqueName = 
+            Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, uniqueName + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
+export default upload;
